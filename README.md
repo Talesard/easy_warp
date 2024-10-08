e4657546c22a9e97be44cea802f9bd0761d09a00
Evgeniy Napylov <61109747+Talesard@users.noreply.github.com>
2024-10-09 01:09:53 +0300

    Small improvements

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 Для запуска у вас должен быть доступен домен cloudflareclient.com (впн или goodbyedpi - неважно)
 
-Легкая генерация WARP конфига для AmneziaWG (https://github.com/amnezia-vpn/amneziawg-windows-client/releases/tag/1.0.0)
-
-скачиваете весь репозиторий и запускайте WARP.exe, у вас появится несколько файлов, вам нужен WARP.conf
+1. Скачать (Releases) и запустить `warp.exe` (на всякий случай от имени администратора)
+2. Получить конфиг
+   1. Включить VPN, когда попросят
+   2. Enter
+   3. Enter
+   4. Добавить WARP.conf в AmneziaWG
+3. VPN больше не понадобится
+4. В дальнейшем запускать только AmneziaWG
--- warp.py
+++ warp.py
@@ -1,18 +1,25 @@
 import os
+import urllib.request
 
-os.system("warp.bat")
+print ("amnezia...")
+urllib.request.urlretrieve ("https://github.com/amnezia-vpn/amneziawg-windows-client/releases/download/1.0.0/amneziawg-amd64-1.0.0.msi", "amneziawg.msi")
+print("wgcf (warp cli)...")
+urllib.request.urlretrieve ("https://github.com/ViRb3/wgcf/releases/download/v2.2.22/wgcf_2.2.22_windows_amd64.exe", "wgcf.exe")
+print ("Turn on any VPN!")
+os.system ("pause")
+print ("Press enter a little later")
+os.system ("wgcf register")
+os.system ("wgcf generate")
 
 with open("wgcf-profile.conf", 'r') as f:
     for line in f:
         if "PrivateKey" in line:
             privatekey = line.split(" ")[2]
-
         if "PublicKey" in line:
             publickey = line.split(" ")[2]
-
-
-
 text = f"[Interface]\nPrivateKey = {privatekey}Jc = 120\nJmin = 23\nJmax = 911\nH1 = 1\nH2 = 2\nH3 = 3\nH4 = 4\nAddress = 172.16.0.2/32, 2606:4700:110:81dd:e13c:3b13:c4e7:855f/128\nDNS = 1.1.1.1, 2606:4700:4700::1111, 1.0.0.1, 2606:4700:4700::1001\nMTU = 1420\n\n[Peer]\nPublicKey = {publickey}AllowedIPs = 0.0.0.0/0, ::/0\nEndpoint = 162.159.193.5:2408"
-
 with open('WARP.conf', 'w') as f:
     f.write(text)
+
+print ("Add WARP.conf to amnezia via GUI")
+os.system ("amneziawg.msi")
